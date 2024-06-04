26360a9d158a3cc7a9ce38dab35d85647b2fe6d5
ahmrh <ahmadrolandi01@gmail.com>
2024-06-04 11:04:09 +0700

    another day

---
+++ leetcode/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+public class RemoveDuplicatesFromSortedArray {
+  
+  public int removeDuplicates(int[] nums) {
+        
+    int count = 0;
+    for(int num: nums){
+        if(num == val) count += 1;
+    }
+    
+    return count;
+  }
+}
