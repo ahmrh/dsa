eac78d9553c667cc3ce2f888097589d8a5e838b0
ahmrh <ahmadrolandi01@gmail.com>
2024-10-03 08:45:57 +0700

    543. Diameter of Binary Tree using python

---
+++ neetcode/BinaryTreeDiameter.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        self.maxDiameter = 0
+        
+        self.diameterCountRecursion(root)
+
+        return self.maxDiameter
+    
+    def diameterCountRecursion(self, node: Optional[TreeNode]) -> int:
+        
+        if node == None:  
+            return 0
+        
+        leftDepth = self.diameterCountRecursion(node.left)
+        rightDepth = self.diameterCountRecursion(node.right)
+
+
+        diameter = leftDepth + rightDepth
+
+        self.maxDiameter = max(self.maxDiameter, diameter)
+
+        return max(leftDepth, rightDepth) + 1
